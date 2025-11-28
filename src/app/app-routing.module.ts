6f885f31b5dabe32efbee6c686ef7504d6b9a463
zekrifarah <Zekri.farah@Esprit.tn>
2025-11-28 14:34:18 +0100

    your message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: 'product', component:HomeComponent},
+  {path: 'suggsetion', component:ListSuggestionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
